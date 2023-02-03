07d8979350da7389771945ccf94d14c73863fd7c
jinbaek <jinmb0602@gmail.com>
2023-02-03 17:03:50 +0900

    feat: add routing when click header menus

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,45 @@
 import { ReactNode } from 'react';
-
+import { FiShoppingBag } from 'react-icons/fi';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
 type HeaderProps = {};
 const Header = ({}: HeaderProps) => {
-  return <div>Header</div>;
+  const navigate = useNavigate();
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+  const handleProductClick = () => {
+    navigate('/products');
+  };
+  const handleCartClick = () => {
+    navigate('/carts');
+  };
+  const handleLoginClick = () => {
+    //callback();
+  };
+  return (
+    <div className="flex justify-between p-2">
+      <div
+        className="flex items-center cursor-pointer text-[#4abad9]"
+        onClick={handleLogoClick}
+      >
+        <FiShoppingBag size={20} />
+        <p className="ml-1">Shoppy</p>
+      </div>
+      <div className=" flex items-center  mr-8">
+        <p className="mr-5 cursor-pointer" onClick={handleProductClick}>
+          Products
+        </p>
+        <AiOutlineShoppingCart
+          className="mr-6 text-xl  font-semibold cursor-pointer"
+          onClick={handleCartClick}
+        />
+        <button className="text-white bg-[#4abad9] " onClick={handleLoginClick}>
+          <p className="p-1 px-3 text-sm text-white font-bold">Login</p>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+// const { colors: defaultColors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
+  theme: {},
   plugins: [],
 };
