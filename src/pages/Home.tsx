ddce3a5fdbb3c00ca6e6d24623843677db8d0a7d
jinbaek <jinmb0602@gmail.com>
2023-03-05 18:24:29 +0900

    styles: styles the  banner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,21 @@
 import { ReactNode } from 'react';
+import AllProducts from './AllProducts';
 
 type HomeProps = {};
 const Home = ({}: HomeProps) => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="flex justify-center items-center w-full   h-72 bg-banner bg-cover bg-center">
+        <span className="text-white">
+          <h1 className=" text-5xl">Shop With Us</h1>
+          <h3 className="text-center font-light my-2">
+            Best Products, High Quality
+          </h3>
+        </span>
+      </div>
+      <AllProducts />
+    </>
+  );
 };
 
 export default Home;
