f0dcc2d049fe195807106a10a22ca87c521232e1
jinbaek <jinmb0602@gmail.com>
2023-03-09 12:30:13 +0900

    feat: move to detail page when click product card and show product detail

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,14 +1,22 @@
 import { ReactNode } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ProductType } from '../pages/NewProduct';
 
 type ProductCardProps = {
   product: ProductType;
 };
-const ProductCard = ({
-  product: { id, image, title, category, price },
-}: ProductCardProps) => {
+const ProductCard = ({ product }: ProductCardProps) => {
+  const { id, image, title, category, price } = product;
+  const navigate = useNavigate();
+  const handleClick = () => {
+    console.log('id: ', id);
+    navigate(`/products/${id}`, { state: { product } });
+  };
   return (
-    <li className="rounded-lg shadow-md overflow-hidden cursor-pointer">
+    <li
+      className="rounded-lg shadow-md overflow-hidden cursor-pointer"
+      onClick={handleClick}
+    >
       <img className="w-full" src={image} alt={title} />
       <div className="mt-2 px-2 text-lg flex  justify-between items-center">
         <h3 className=" truncate">{title}</h3>
--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,8 +1,34 @@
 import { ReactNode } from 'react';
+import { useLocation } from 'react-router-dom';
+import Button from '../components/ui/Button';
 
 type ProductDetailProps = {};
 const ProductDetail = ({}: ProductDetailProps) => {
-  return <div></div>;
+  const { category, price, description, options, image, id, title } =
+    useLocation().state.product;
+
+  return (
+    <>
+      <p>{category}</p>
+      <div>
+        <img src={image} alt="clothes" />
+        <div>
+          <p>{title}</p>
+          <p>{`₩${price}`}</p>
+          <p>{description}</p>
+          <div>
+            <label htmlFor="options">Options</label>
+            <select id="options">
+              {options.map((option: string) => (
+                <option value={option}>{option}</option>
+              ))}
+            </select>
+          </div>
+          <Button text="Add to Cart" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProductDetail;
