e67613573d93e1b71e3d796ef406bbf39cbdac6a
jinbaek <jinmb0602@gmail.com>
2023-03-10 22:51:30 +0900

    feat: show the product detail when a product clicked

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,14 +1,22 @@
 import { ReactNode } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ProductType } from '../pages/NewProduct';
 
 type ProductCardProps = {
   product: ProductType;
 };
 const ProductCard = ({
+  product,
   product: { id, image, title, category, price },
 }: ProductCardProps) => {
+  const navigate = useNavigate();
   return (
-    <li className="rounded-lg shadow-md overflow-hidden cursor-pointer">
+    <li
+      onClick={() => {
+        navigate(`/products/${id}`, { state: { product } });
+      }}
+      className="rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105"
+    >
       <img className="w-full" src={image} alt={title} />
       <div className="mt-2 px-2 text-lg flex  justify-between items-center">
         <h3 className=" truncate">{title}</h3>
--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,8 +1,53 @@
-import { ReactNode } from 'react';
+import { ChangeEvent, ReactNode, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import Button from '../components/ui/Button';
 
 type ProductDetailProps = {};
 const ProductDetail = ({}: ProductDetailProps) => {
-  return <div></div>;
+  const {
+    state: {
+      product: { id, image, title, description, category, price, options },
+    },
+  } = useLocation();
+  const [selected, setSelected] = useState<string>(options && options[0]);
+  console.log('selected: ', selected);
+  const handleSelect = (e: ChangeEvent<HTMLSelectElement>) =>
+    setSelected(e.target.value);
+  const handleClick = () => {
+    // Add to Cart logic here
+  };
+  return (
+    <>
+      <p className="mx-12 mt-4 text-gray-700">{category}</p>
+      <section className="flex flex-col md:flex-row p-4">
+        <img className="w-full px-4 basis-7/12" src={image} alt={title} />
+        <div className="w-full basis-5/12 flex flex-col p-4">
+          <h2 className="text-3xl font-bold py-2 ">{title}</h2>
+          <p className="text-2xl font-bold py-2 border-b border-gray-400">
+            ₩{price}
+          </p>
+          <p className="py-4 text-lg">{description}</p>
+          <div className="flex items-center">
+            <label className="text-brand font-bold" htmlFor="select">
+              options:
+            </label>
+            <select
+              id="select"
+              className="p-2 m-4 flex-1 border-2 border-dashed border-brand outline-none"
+              onChange={handleSelect}
+              value={selected}
+            >
+              {options &&
+                options.map((option: string, index: number) => (
+                  <option key={index}>{option}</option>
+                ))}
+            </select>
+          </div>
+          <Button text="Add to Cart" onClick={handleClick} />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default ProductDetail;
