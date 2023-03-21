a0ad7dfce26419e715530e1b56af1d9852031895
jinbaek <jinmb0602@gmail.com>
2023-03-21 12:57:05 +0900

    styles: styles cart page

--- src/components/PriceCard.tsx
+++ src/components/PriceCard.tsx
@@ -5,7 +5,12 @@ type PriceCardProps = {
   price: number;
 };
 const PriceCard = ({ text, price }: PriceCardProps) => {
-  return <div></div>;
+  return (
+    <div className="bg-gray-50 p-8 mx-2 rounded-2xl text-center text-lg md:text-xl">
+      <p>{text}</p>
+      <p className="font-bold text-brand text-xl md:text-2xl">₩{price}</p>
+    </div>
+  );
 };
 
 export default PriceCard;
--- src/pages/MyCart.tsx
+++ src/pages/MyCart.tsx
@@ -6,6 +6,7 @@ import CartItem from '../components/CartItem';
 import PriceCard from '../components/PriceCard';
 import { BsFillPlusCircleFill } from 'react-icons/bs';
 import { FaEquals } from 'react-icons/fa';
+import Button from '../components/ui/Button';
 
 const SHIPPING = 3000;
 export type CartProduct = {
@@ -35,26 +36,29 @@ const MyCart = ({}: MyCartProps) => {
     : 0;
 
   return (
-    <section>
-      <p>My Cart</p>
+    <section className="p-8 flex flex-col">
+      <p className="text-2xl text-center font-bold pb-4 border-b border-gray-300">
+        My Cart
+      </p>
       {!hasProducts && (
         <p>There is no products in your cart. Please do your best shopping!</p>
       )}
       {hasProducts && (
         <>
-          <ul>
+          <ul className="border-b border-gray-300 mb-8 p-4 px-8">
             {products &&
               products.map((product) => (
                 <CartItem key={product.id} product={product} uid={uid} />
               ))}
           </ul>
-          <div>
+          <div className="flex justify-between items-center mb-6 px-2 md:px-8 lg:px-16">
             <PriceCard text="Total Price" price={totalPrice} />
-            <BsFillPlusCircleFill />
+            <BsFillPlusCircleFill className="shrink-0" />
             <PriceCard text="Shipping Fee" price={SHIPPING} />
-            <FaEquals />
+            <FaEquals className="shrink-0" />
             <PriceCard text="Final Price" price={totalPrice + SHIPPING} />
           </div>
+          <Button text="Place Order" />
         </>
       )}
     </section>
